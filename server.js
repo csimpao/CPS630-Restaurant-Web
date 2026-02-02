fce3454558d0b1691f40e227823a8e9b5906f019
csimpao <csimpao@torontomu.ca>
2026-02-02 08:09:06 -0500

    modified server.js

--- server.js
+++ server.js
@@ -10,4 +10,12 @@ app.get('/menu', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/menu.html'));
 });
 
+app.get('/end', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/ending.html'));
+});
+
+app.get('/receipt', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/receipt.html'));
+});
+
 app.listen(PORT, () => { console.log("Server started on port: " + PORT) });
